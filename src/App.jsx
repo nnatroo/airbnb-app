eea37bbd56aa366a682753ccef0465789fdeec35
nnatroo <nugonatroshvili@gmail.com>
2023-03-13 04:40:47 +0400

    Co-authored-by: LukaOrjonikidze <LukaOrjonikidze@users.noreply.github.com> Co-authored-by: NikolozTsutskhubaia <NikolozTsutskhubaia@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import './Components/Modules/App.module.css';
 import Navbar from './Components/Navigation/Navbar/Navbar'
 import apartaments from './apartaments';
 import Apartaments from './Components/Apartament/Apartaments';
+import Footer from './Components/Footer/Footer';
+
+import './Components/Modules/App.module.css'
 
 function App() {
   return (<>
         <Navbar />
         <Apartaments items={apartaments}/>
+        <Footer/>
   </>
   );
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import classes from "../Modules/Footer.module.css";
+
+const Footer = () => {
+    return <>
+        <footer>
+            <div>
+                <li>© 2023 Airbnb, Inc.</li>
+                <li>Terms</li>
+                <li>Sitemap</li>
+                <li>Privacy</li>
+                <li>Your Privacy Choices</li>
+                <li>Destinations</li>
+            </div>
+            <div>
+                <li>English (US)</li>
+                <li>$ USD</li>
+                <li>Support & resources</li>
+            </div>
+        </footer>
+    </>
+}
+export default Footer;
