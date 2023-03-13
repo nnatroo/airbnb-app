a78118022bb5d888d7f33af70d7bb9ee4ab11f56
nnatroo <nugonatroshvili@gmail.com>
2023-03-13 07:15:56 +0400

    Fixes #100

--- src/Components/Navigation/Navbar/Navbar.jsx
+++ src/Components/Navigation/Navbar/Navbar.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import AirbnbIcon from "../../UI/AirbnbIcon";
 import Search from "../Search/Search";
-import UserControls from '../UserControls/UserControls'
+import UserControls from "../UserControls/UserControls";
+import SearchExt from "../Search/SearchExt";
 // import LearnMore from "../LearnMore/LearnMore";
 import "../../Modules/Navbar.module.css";
+import { useState } from "react";
 
-function Navbar(){
-    return (
-        <nav>
-            <AirbnbIcon />
-            <Search />
-            <UserControls />
-        </nav>
-    )
+function Navbar() {
+  const [isActive, setIsActive] = useState(false);
+
+  const activeHandler = () => {
+    setIsActive(!isActive);
+  };
+  
+  return (
+    // style={{paddingBottom: isActive ? '4rem' : '0.7rem'}}
+    <nav style={{ paddingBottom: isActive ? "10rem" : "0.7rem" }}>
+      <AirbnbIcon />
+      <Search isActive={activeHandler} />
+      {if(isActive) }
+      <UserControls />
+    </nav>
+  );
 }
 
 export default Navbar;
